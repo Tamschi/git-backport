c2d9cec5eb3792d601701f632e39fd2a91d371ad
Tamme Schichler <tamme@schichler.dev>
2020-03-24 15:53:38 +0100

    Refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -193,6 +193,84 @@ pub fn backport<E: FnOnce(&[Branch], &[BackportCommit])>(
             *dirty = true;
         }
     }
+
+    fn catch_up_branch<'a>(
+        branch_index: usize,
+        branches: &[Branch],
+        heads: &mut [Option<Commit<'a>>],
+        inverse_map: &mut HashMap<Oid, Commit<'a>>,
+        branch_map_overlays: &mut [HashMap<Oid, Commit<'a>>],
+        dirty: &mut [bool],
+        repository: &'a Repository,
+    ) -> Oid {
+        if branch_index == branches.len() - 1 || !dirty[branch_index] {
+            return inverse_map[&heads[branch_index].as_ref().unwrap().id()].id();
+        }
+        let original_commit_id = catch_up_branch(
+            branch_index + 1,
+            branches,
+            heads,
+            inverse_map,
+            branch_map_overlays,
+            dirty,
+            repository,
+        );
+        trace!("Catching up branch {}...", branch_index);
+        heads[branch_index] = Some(match heads[branch_index].as_ref() {
+            None => heads[branch_index + 1].as_ref().unwrap().clone(),
+            Some(head) => {
+                let mut merge_index = repository
+                    .merge_commits(
+                        head,
+                        heads[branch_index + 1].as_ref().unwrap(),
+                        Some(
+                            MergeOptions::new()
+                                .find_renames(true)
+                                .fail_on_conflict(true)
+                                .minimal(true),
+                        ),
+                    )
+                    .expect(
+                        "This should never fail, since the changes were compatible to begin with.",
+                    );
+                let merge_oid = merge_index.write_tree().unwrap();
+                let merge_tree = repository.find_tree(merge_oid).unwrap();
+                let signature = repository
+                    .signature()
+                    .expect("Could not create default signature");
+                let merge_commit_id = repository
+                    .commit(
+                        None,
+                        &signature,
+                        &signature,
+                        &format!(
+                            "Merge {} into {}",
+                            branches[branch_index + 1].name().unwrap().unwrap(),
+                            branches[branch_index].name().unwrap().unwrap(),
+                        ),
+                        &merge_tree,
+                        &[head, heads[branch_index + 1].as_ref().unwrap()],
+                    )
+                    .unwrap();
+                repository.find_commit(merge_commit_id).unwrap()
+            }
+        });
+        assert!(branch_map_overlays[branch_index]
+            .insert(
+                original_commit_id,
+                heads[branch_index].as_ref().unwrap().clone()
+            )
+            .is_none());
+        assert!(inverse_map
+            .insert(
+                heads[branch_index].as_ref().unwrap().id(),
+                repository.find_commit(original_commit_id).unwrap()
+            )
+            .is_none());
+        dirty[branch_index] = false;
+        original_commit_id
+    }
+
     for commit_parent in commits.windows(2).rev() {
         let (commit, parent) = match commit_parent {
             [commit, parent] => (commit, parent),
@@ -207,80 +285,9 @@ pub fn backport<E: FnOnce(&[Branch], &[BackportCommit])>(
             dirty.as_mut_slice(),
             repository,
         );
-        fn catch_up_branch<'a>(
-            branch_index: usize,
-            branches: &[Branch],
-            heads: &mut [Option<Commit<'a>>],
-            inverse_map: &mut HashMap<Oid, Commit<'a>>,
-            branch_map_overlays: &mut [HashMap<Oid, Commit<'a>>],
-            dirty: &mut [bool],
-            repository: &'a Repository,
-        ) -> Oid {
-            if branch_index == branches.len() - 1 || !dirty[branch_index] {
-                return inverse_map[&heads[branch_index].as_ref().unwrap().id()].id();
-            }
-            let original_commit_id = catch_up_branch(
-                branch_index + 1,
-                branches,
-                heads,
-                inverse_map,
-                branch_map_overlays,
-                dirty,
-                repository,
-            );
-            trace!("Catching up branch {}...", branch_index);
-            heads[branch_index] = Some(match heads[branch_index].as_ref() {
-                None => heads[branch_index + 1].as_ref().unwrap().clone(),
-                Some(head) => {
-                    let mut merge_index = repository.merge_commits(
-                            head,
-                            heads[branch_index + 1].as_ref().unwrap(),
-                            Some(
-                                MergeOptions::new()
-                                    .find_renames(true)
-                                    .fail_on_conflict(true)
-                                    .minimal(true),
-                            ),
-                        ).expect("This should never fail, since the changes were compatible to begin with.");
-                    let merge_oid = merge_index.write_tree().unwrap();
-                    let merge_tree = repository.find_tree(merge_oid).unwrap();
-                    let signature = repository
-                        .signature()
-                        .expect("Could not create default signature");
-                    let merge_commit_id = repository
-                        .commit(
-                            None,
-                            &signature,
-                            &signature,
-                            &format!(
-                                "Merge {} into {}",
-                                branches[branch_index + 1].name().unwrap().unwrap(),
-                                branches[branch_index].name().unwrap().unwrap(),
-                            ),
-                            &merge_tree,
-                            &[head, heads[branch_index + 1].as_ref().unwrap()],
-                        )
-                        .unwrap();
-                    repository.find_commit(merge_commit_id).unwrap()
-                }
-            });
-            assert!(branch_map_overlays[branch_index]
-                .insert(
-                    original_commit_id,
-                    heads[branch_index].as_ref().unwrap().clone()
-                )
-                .is_none());
-            assert!(inverse_map
-                .insert(
-                    heads[branch_index].as_ref().unwrap().id(),
-                    repository.find_commit(original_commit_id).unwrap()
-                )
-                .is_none());
-            dirty[branch_index] = false;
-            original_commit_id
-        }
 
         todo!("Cherry-pick while mapping parent commits (especially side chains)");
+        
         for dirty in dirty[0..*commit.branch_index.borrow()].iter_mut() {
             *dirty = true;
         }
